61ce2fc2feae67ae9bac4e780169460befe1486d
bsboris <bsboris@gmail.com>
2015-08-26 11:14:44 +0300

    Rename function ranked_get_product_with_rank_in_category

--- test/get_position_for_rank_in_category_test.rb
+++ test/get_position_for_rank_in_category_test.rb
@@ -10,22 +10,22 @@ class GetPositionForRankInCategoryTest < MiniTest::Test
     @id_d = create_product("D", [1], { 1 => 30000 })
   end
 
-  def get_product_with_rank_in_category(category, rank)
-    exec_first("SELECT ranked_get_product_with_rank_in_category(#{category}, #{rank}) as rank")["rank"]
+  def get_position_for_rank_in_category(category, rank)
+    exec_first("SELECT ranked_get_position_for_rank_in_category(#{category}, #{rank}) as rank")["rank"]
   end
 
   def test_gets_product
-    assert_equal "0", get_product_with_rank_in_category(1, 1)
-    assert_equal "10000", get_product_with_rank_in_category(1, 2)
-    assert_equal "20000", get_product_with_rank_in_category(1, 3)
-    assert_equal "30000", get_product_with_rank_in_category(1, 4)
+    assert_equal "0", get_position_for_rank_in_category(1, 1)
+    assert_equal "10000", get_position_for_rank_in_category(1, 2)
+    assert_equal "20000", get_position_for_rank_in_category(1, 3)
+    assert_equal "30000", get_position_for_rank_in_category(1, 4)
   end
 
   def test_zero
-    assert_equal nil, get_product_with_rank_in_category(1, 0)
+    assert_equal nil, get_position_for_rank_in_category(1, 0)
   end
 
   def test_non_existing_rank
-    assert_equal nil, get_product_with_rank_in_category(1, 999)
+    assert_equal nil, get_position_for_rank_in_category(1, 999)
   end
 end
